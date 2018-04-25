b4ea6ccb0585c14bbcc4c6ce931fc099a3ddc789
noelle none <noe.ililonga@gmail.com>
2018-04-25 08:50:52 -0400

    First Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare an integer
+            int answer;
+
+            //have user enter a number
+            Console.WriteLine("Enter a number between 1 and 100:");
+            answer = int.Parse(Console.ReadLine());
+
+            //determine even numbers between the range of 2 to 25
+            if  (answer <= 25 && answer > 2 & answer % 2 == 0)
+            {
+                Console.WriteLine(answer + " is an even number and less than 25");
+            
+            //determine even numbers between the range of 26 to 60
+            } else if (answer >= 26 && answer <= 60 & answer % 2 == 0) 
+            
+            {
+                Console.WriteLine(" Even");
+            }
+            //determine even numbers greater than 60
+            else if (answer > 60 & answer % 2 == 0)
+            {
+                Console.WriteLine(answer + " is Even");
+            }
+            //determine odd numbers greater than 60
+            else if (answer % 2 == 0 & answer > 60)
+            {
+                Console.WriteLine(answer + " is odd");
+            }
+
+            else
+            {
+                Console.WriteLine(answer + " is Odd");
+            }
+
+            Console.WriteLine("Press any key to close the program...");
+            Console.ReadKey(true);
+
+
+
+
+        }
+    }
+}
